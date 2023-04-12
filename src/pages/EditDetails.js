89022edaf1093239d8cf3f6c36c9344af494bca1
mayurinid <rnmayuri08@gmail.com>
2023-04-12 17:20:55 +0530

    first commit

--- src/pages/EditDetails.js
+++ src/pages/EditDetails.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import { useLocation,useNavigate } from 'react-router-dom';
 import { useSelector,useDispatch } from 'react-redux';
-import { studentAdded, studentUpdated } from '../feature/StudentSlice';
+import { studentUpdated } from '../feature/StudentSlice';
 
 
 const EditDetails = () => {
